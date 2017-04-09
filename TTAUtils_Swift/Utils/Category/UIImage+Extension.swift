ac9f9090040ba8ce332b40ab3cf708f1ee2f29ae
TobyoTenma <tmtbo@hotmail.com>
2017-04-09 22:24:52 +0800

    1.添加创建二维码功能

--- TTAUtils_Swift/Utils/Category/UIImage+Extension.swift
+++ TTAUtils_Swift/Utils/Category/UIImage+Extension.swift
@@ -16,7 +16,7 @@ extension TTAUtils where Base: UIImage {
     ///   - color: 颜色
     ///   - size: 大小
     /// - Returns: 图片
-    open class func image(color: UIColor, size: CGSize = CGSize(width: 0.1, height: 0.1)) -> UIImage? {
+    class func image(color: UIColor, size: CGSize = CGSize(width: 0.1, height: 0.1)) -> UIImage? {
         UIGraphicsBeginImageContextWithOptions(size, false, UIScreen.main.scale)
         let ctx = UIGraphicsGetCurrentContext()
         ctx?.setFillColor(color.cgColor)
@@ -32,7 +32,7 @@ extension TTAUtils where Base: UIImage {
     ///   - frame: 图片大小
     ///   - isCircleImage: 是否圆形
     ///   - completion: 完成回调
-    open func image(frame: CGRect, isCircleImage: Bool, completion: @escaping (UIImage?) -> Void) {
+    func image(frame: CGRect, isCircleImage: Bool, completion: @escaping (UIImage?) -> Void) {
         DispatchQueue.global().async {
             UIGraphicsBeginImageContextWithOptions(frame.size, false, UIScreen.main.scale)
             if isCircleImage {
@@ -51,13 +51,56 @@ extension TTAUtils where Base: UIImage {
     /// adjust the image orientation
     ///
     /// - Returns: adjusted image
-    open func normalizedImage() -> UIImage {
+    func normalizedImage() -> UIImage {
         guard base.imageOrientation == UIImageOrientation.up else { return base }
         UIGraphicsBeginImageContextWithOptions(base.size, false, base.scale)
         base.draw(in: CGRect(x: 0, y: 0, width: base.size.width, height: base.size.height))
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return image ?? base
+    }
+}
+
+// MARK: - QRCode
+
+extension TTAUtils where Base: UIImage {
+    struct QRCodeCenterImage {
+        var image: UIImage
+        var ratio: CGFloat
+        
+        static private func _validateRatio(_ ratio: CGFloat) -> Bool {
+            return ratio <= 0.4
+        }
+        
+        init(image: UIImage, ratio: CGFloat) {
+            self.image = image
+            self.ratio = QRCodeCenterImage._validateRatio(ratio) ? ratio : 0.4
+        }
+    }
+    
+    class func createQRCode(_ content: Data?, center image: QRCodeCenterImage?) -> UIImage {
+        let filter = CIFilter(name: "CIQRCodeGenerator")
+        filter?.setDefaults()
+        filter?.setValue(content, forKey: "inputMessage")
+        filter?.setValue("H", forKey: "inputCorrectionLevel")
+        guard let ciimage = filter?.outputImage?.applying(CGAffineTransform(scaleX: 10, y: 10)) else { return UIImage() }
+        let qrcodeImage = UIImage(ciImage: ciimage)
+        guard let centerImage = image else { return qrcodeImage }
+        return _drawCenterImage(in: qrcodeImage, center: centerImage)
+    }
+    
+    class private func _drawCenterImage(in qrcodeImage: UIImage, center image: QRCodeCenterImage) -> UIImage {
+        // draw center image
+        UIGraphicsBeginImageContextWithOptions(qrcodeImage.size, false, UIScreen.main.scale)
+        let size = qrcodeImage.size
+        let centerImageSize = CGSize(width: image.ratio * size.width, height: image.ratio * size.height)
+        qrcodeImage.draw(in: CGRect(origin: .zero, size: qrcodeImage.size))
+        let x = (qrcodeImage.size.width - centerImageSize.width) / 2
+        let y = (qrcodeImage.size.height - centerImageSize.height) / 2
+        image.image.draw(in: CGRect(origin: CGPoint(x: x, y: y), size: centerImageSize))
+        guard let newQRCodeImage = UIGraphicsGetImageFromCurrentImageContext() else { return qrcodeImage }
+        UIGraphicsEndImageContext()
+        return newQRCodeImage
     }
 }
 
